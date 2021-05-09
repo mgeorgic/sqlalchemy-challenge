a6a1db5c51f3eab152989d82447e2842ae127d45
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-05-09 14:53:25 -0600

    Completed welcome route and db and flask setup

--- HW/Code/app.py
+++ HW/Code/app.py
@@ -19,17 +19,45 @@ Base.prepare(engine, reflect=True)
 # Save reference to the tables
 MS = Base.classes.measurement
 ST = Base.classes.station
+#print(Base.classes.keys())
 
-# Start session
-session = Session(engine)
-
-# Return the lastest date within the database
-last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-
-# Return the date 1 year ago from the lastest date in the database
-query_date = dt.date(2017,8,23) - dt.timedelta(days=365)
-
-session.close()
-
-# Create an app
+# Setup the Flask
 app = Flask(__name__)
+
+# Flask Routes ("/")
+# Define what to do when user hits the index route
+@app.route("/")
+def home():
+    """List of all the available api routes."""
+    return(
+        f"Welcome to the Climate App API.<br>"
+        f"Here are the available routes:<br/>"
+        f"<br/>"  
+        f"Precipitation data:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"<br/>"
+        f"Stations:<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"<br/>"
+        f"Temperture observations:<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"<br/>"
+        f"/api/v1.0/start<br/>"
+        f"<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"<br/>"
+    )
+###########################################################
+
+# create precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Retrieve the last 12 months of precipitation data and return the results."""
+    # Create the session link
+    session = Session(engine)
+    
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(MS, MS.prcp).\
+        filter(MS.date >= date).all()
+
+python app.py
