fbda4222ae367fb1c61cc3da813fe4ffdbe2ad3d
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-05-14 11:52:04 -0600

    added session link starts and ends to each section to avoid loading webpage errors. Fixed the conditional if statement for start and end date temps portion to clean up extra code.

--- HW/app.py
+++ HW/app.py
@@ -1,12 +1,10 @@
 # Import Dependencies
 import numpy as np
 import datetime as dt
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 # Database Setup
@@ -29,10 +27,15 @@ session = Session(engine)
 app = Flask(__name__)
 
 # Flask Routes ("/")
-# Define what to do when user hits the index route
+# Create a list of URL options for the user
 @app.route("/")
 def home():
     """List of all the available api routes."""
+    
+    # Open session 
+    session = Session(engine)
+
+    # URL string list
     return(f'''
         Welcome to the Climate App API!<br>
         <br/>
@@ -54,12 +57,17 @@ def home():
         /api/v1.0/start/end<br/>
         <br/>''')
 
+session.close()
 ###########################################################
 
 # Precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Retrieve the last 12 months of precipitation data and return the results."""
+    
+    # Open session
+    session = Session(engine)
+
     year_date = dt.date(2017,8,23) - dt.timedelta(days=365)
     prcp = session.query(MS.date, MS.prcp).\
         filter(MS.date >= year_date).\
@@ -67,12 +75,20 @@ def precipitation():
     prcp_dict = dict(prcp)
     return jsonify(prcp_dict)
 
-# create station route
+session.close()
+
+# Station route
 @app.route("/api/v1.0/stations")
 def stations():
     """Return a list of stations"""
+    
+    # Open session link
+    session = Session(engine)
+
     results = session.query(ST.station, ST.name).all()
-     # Convert list of tuples into list of dictionaries for each station and name
+
+    session.close()
+    # Convert list of tuples into list of dictionaries for each station and name
     station_list = []
     for result in results:
         r = {}
@@ -87,10 +103,14 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Return a list of temperature observations for the previous year"""
+    # create session link
+    session = Session(engine)
+
     query_date = dt.date(2017,8,23) - dt.timedelta(days=365)
     results = session.query(MS.tobs, MS.date).\
         filter(MS.date >= query_date).all()
     
+    session.close()
     # convert list of tuples to show dates and temps
     tobs_list = []
     for result in results:
@@ -98,46 +118,26 @@ def tobs():
         r["Date"] = result[1]
         r["Recorded Temp"] = result[0]
         tobs_list.append(r)
-    return jsonify(tobs_list = tobs_list)
+    return jsonify(tobs_list)
  
 @app.route("/api/v1.0/<start>")
-#@app.route("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/<start>/<end>")
 def start (start=None, end=None):
-    
-    """Return a JSON list of tmin, tmax, tavg for the dates greater than or equal to the date provided"""
-    
+    """Return a JSON list of temp min, temp max, temp avg for the dates greater than or equal to the date/s provided"""
+    # create session link
+    session = Session(engine)
+
     # Convert start and end dates to yyyy-mm-dd format for the query
     start_dt = dt.datetime.strptime(start, '%Y-%m-%d')
-    #end_dt = dt.datetime.strptime(end, '%Y-%m-%d')
+    end_dt = dt.datetime.strptime(end, '%Y-%m-%d')
     
     if not end:
         results = session.query(MS.date, func.min(MS.tobs), func.avg(MS.tobs),
         func.max(MS.tobs)).filter(MS.date >= start_dt).all()
-    # if end:
-    #     results = session.query(MS.date, func.min(MS.tobs), func.avg(MS.tobs),
-    #     func.max(MS.tobs)).filter(MS.date >= start).filter(MS.date <= end_dt).all()
-    
-    # Create a list to hold results
-    temp_list = []
-    for result in results:
-        r = {}
-        r["Actual Date"] = result[0]
-        r["Temp Min"] = result[1]
-        r["Temp Avg"] = result[2]
-        r["Temp Max"] = result[3]
-        temp_list.append(r)
-    return jsonify(temp_list)
-
-@app.route("/api/v1.0/<start>/<end>")
-def start_end(start=None, end=None):
-
-    """Return a JSON list of tmin, tmax, tavg for the dates in range of start date and end date inclusive"""
-    
-    # take start and end dates and convert to yyyy-mm-dd format for the query
-    start_dt = dt.datetime.strptime(start, '%Y-%m-%d')
-    end_dt = dt.datetime.strptime(end, "%Y-%m-%d")
-    
-    results = session.query(MS.date, func.min(MS.tobs), func.avg(MS.tobs),func.max(MS.tobs)).filter(MS.date <= end).filter(MS.date >= start).group_by(MS.date).all()
+        session.close()
+    if end:
+        results = session.query(MS.date, func.min(MS.tobs), func.avg(MS.tobs),
+        func.max(MS.tobs)).filter(MS.date <= end_dt).filter(MS.date >= start_dt).group_by(MS.date).all()
     
     # Create a list to hold results
     temp_list = []
